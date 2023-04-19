75d92926cdd58c6602ee6f65386765331a43a727
Erick_Verissim0 <estudodoerick@gmail.com>
2023-04-19 14:46:27 -0300

    personalized errors

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -32,3 +32,35 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Erick',
+        email: 'erick@gmail.com',
+        passwordConfirmation: '4002908922'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+})
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no confirmPassword is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Erick',
+        email: 'erick@gmail.com',
+        password: '40028922'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('confirmPassword'))
+  })
+})
